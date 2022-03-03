9709d6a89823937347c03f4664218ddf10eea078
unknown <chilamakooru.gangaiiit@gmail.com>
2022-03-03 23:09:30 +0530

    Creating_Stck

--- Program.cs
+++ Program.cs
@@ -6,6 +6,18 @@ namespace Stack_Problems
         public static void Main()
         {
             Console.WriteLine("Welcpme to Stack Problems");
+            Stackcs stacks = new Stackcs();
+            
+
+            Node node1 = new Node(70);
+            Node node2 = new Node(30);
+            Node node3 = new Node(56);
+
+            stacks.Push(node1);
+            stacks.Push(node2);
+            stacks.Push(node3);
+
+            stacks.Display();
         }
     }
 }
---
+++ Stackcs.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_Problems
+{
+    internal class Stackcs
+    {
+       
+       public Node Tail = null;
+
+        public void Push(Node data)
+        {
+            if (Tail == null)
+            {
+                Tail = data;
+            }
+            else
+            {
+                Node temp = data;
+                temp.next = Tail;
+                Tail = temp;
+
+            }
+
+        }
+        public void Display()
+        {
+            if (Tail == null)
+            {
+                Console.WriteLine("Stack is empty");
+            }
+            else
+            {
+                Node p = Tail;
+
+                while (p != null)
+                {
+                    Console.WriteLine(p.data);
+                    p = p.next;
+                }
+            }
+        }
+    }
+}
