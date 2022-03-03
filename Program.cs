be6f834b5f7ea1eb9c9c9b8970a48716c965ed63
Gangadevi Ch <chilamakooru.gangaiiit@gmail.com>
2022-03-03 22:42:58 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Stack_Problems
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Welcpme to Stack Problems");
+        }
+    }
+}
